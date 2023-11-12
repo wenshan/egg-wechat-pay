de9f82bf785a1f722f2eb43a830a3e5e1d546469
wenshan <hou_ve@hotmail.com>
2023-11-12 21:42:42 +0800

    init

---
+++ config/config.default.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * 微信公众平台的相关配置
+ * @member Config Egg配置
+ * @property {String}  appId - 应用号
+ * @property {number}  appSecret  - 应用密钥
+ * @property {number}  mchId  - 商户平台商家编号
+ * @property {number}  apiKey  - 商户支付接口密钥
+ * @property {number}  notifyUrl  - 支付结果回调地址
+ */
+exports.mp = {
+  appId: '',
+  appSecret: '',
+  mchId: '',
+  apiKey: '',
+  notifyUrl: '',
+};
